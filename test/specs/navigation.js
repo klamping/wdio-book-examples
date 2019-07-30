2fb6cb9f841a4f0e92453a70455423adebae42f6
Kevin Lamping <klamping@gmail.com>
2019-07-29 22:13:43 -0500

    Exercise 1-3

--- test/specs/navigation.js
+++ test/specs/navigation.js
@@ -1,20 +1,17 @@
+const assert = require('assert');
+
 describe('Homepage', function () {
   it('should load properly', function () {
     // load the page
     browser.url('./');
 
     // Get the title of the homepage, should be 'Conduit'
-    if (browser.getTitle() !== 'The wrong title') {
-       // throw an error explaining what went wrong
-       throw new Error('Title of the homepage should be "Conduit"')
-    }
+    assert.strictEqual(browser.getTitle(), 'Conduit');
 
     // Click the 'Sign in' navigation link
     $('=Sign in').click();
 
     // Get the URL of the sign in page. It should include 'login'
-    if (browser.getUrl() !== 'https://demo.learnwebdriverio.com/login') {
-      throw new Error('URL of "login" page should be correct')
-    }
+    assert.strictEqual(browser.getUrl(), 'https://demo.learnwebdriverio.com/login');
   })
 })
